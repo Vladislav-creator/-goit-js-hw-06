caebf8993b882450ddc7d2fcb43be267dbb52b7a
Vladislav-creator <valdisbeketov@gmail.com>
2023-08-09 17:25:25 +0300

    8

--- js/task-06.js
+++ js/task-06.js
@@ -1,24 +1,24 @@
-document.getElementById("validation-input").onblur = function () {
-  console.log(this.value.length);
-  if (Number(this.getAttribute("data-length")) !== this.value.length) {
-    this.classList.remove("valid");
-    this.classList.add("invalid");
-  } else {
-    this.classList.remove("invalid");
-    this.classList.add("valid");
-  }
-};
-
-// const nameInput = document.querySelector("#validation-input");
-// console.log(nameInput);
-// nameInput.addEventListener("input", onInput);
-// function onInput(evt) {
-//   console.log(evt.currentTarget.value);
-//   if (evt.currentTarget.value.length === 6) {
-//     nameInput.classList.add("invalid");
-//     console.log("yes");
+// document.getElementById("validation-input").onblur = function () {
+//   console.log(this.value.length);
+//   if (Number(this.getAttribute("data-length")) !== this.value.length) {
+//     this.classList.remove("valid");
+//     this.classList.add("invalid");
 //   } else {
-//     console.log("no");
+//     this.classList.remove("invalid");
+//     this.classList.add("valid");
 //   }
-// }
-// console.log(document.querySelector("style"));
+// };
+
+//The second option
+// const nameInput = document.querySelector("#validation-input");
+// nameInput.onblur = function () {
+//   if (
+//     nameInput.value.length === Number(nameInput.getAttribute("data-length"))
+//   ) {
+//     nameInput.classList.add("valid");
+//     nameInput.classList.remove("invalid");
+//   } else if (nameInput.value.length >= 1) {
+//     nameInput.classList.add("invalid");
+//     nameInput.classList.remove("valid");
+//   }
+// };
--- js/task-07.js
+++ js/task-07.js
@@ -5,3 +5,11 @@ function handleInputRange(event) {
   span.style.fontSize = event.currentTarget.value + "px";
   console.log(span.style.fontSize);
 }
+//2 option
+// const input = document.querySelector("#font-size-control");
+// const span = document.querySelector("#text");
+// input.addEventListener("input", handleInputRange);
+// function handleInputRange() {
+//   span.style.fontSize = input.value + "px";
+//   console.log(span.style.fontSize);
+// }
